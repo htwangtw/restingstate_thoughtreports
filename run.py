014bd889cca9db9a8214eef3c43f5c5fca131f64
Hao-Ting Wang <haoting.wang@york.ac.uk>
2017-11-02 13:47:14 +0000

    New resting state scan experiment

---
+++ run.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+'''run.py
+build the main program here
+'''
+
+import os
+import sys
+
+from psychopy import core, event, logging, visual
+from random import shuffle
+from src.experiment import subject_info, event_logger, Paradigm, Text, Question, instructions
+from src.fileIO import load_conditions_dict, write_csv, read_only
+
+INFO = {
+    'Experiment': 'resting_state',  # compulsory
+    'Subject': 'R0001_001',  # compulsory
+    }
+settings = {
+    'window_size': ,'full_screen',
+    #'window_size': [800, 600],
+    'mouse_visible': False,
+    'logging_level': logging.INFO
+}
+
+# MRI related settings
+dummy_vol = 3
+tr = 3
+slice_per_vol = 60
+RS_length = 9
+
+# RSQ path
+PATH = '/groups/Projects/P1336/resting_state/resting_Q.csv'
+questions, headers = load_conditions_dict(PATH)
+headers += ['StartTime', 'Rating', 'RT', 'IDNO']
+shuffle(questions)
+
+RSQ_txt = '/groups/Projects/P1336/resting_state/instructions/RSQ_instr.txt'
+end_txt = '/groups/Projects/P1336/resting_state/instructions/end_instr.txt'
+# collect participant info
+experiment_info = subject_info(INFO)
+# now run this thing
+if __name__ == "__main__":
+    # set working directory as the location of this file
+    _thisDir = os.path.dirname(os.path.abspath(__file__)
+                               ).decode(sys.getfilesystemencoding())
+    os.chdir(_thisDir)
+
+    # set log file
+    event_logger(settings['logging_level'], experiment_info['LogFile'])
+
+    # create experiment
+    Experiment = Paradigm(escape_key='esc', color=0,
+                          window_size=settings['window_size'])
+    fixation = Text(window=Experiment.window, text='+', color='black')
+    trigger = visual.TextStim(
+                Experiment.window, text='Ready...',
+                name='trigger',
+                pos=[-50,0], height=34, wrapWidth=1100,
+                color='black',
+                )
+    question = Question(window=Experiment.window, questions=questions, color='black')
+    RSQ = instructions(window=Experiment.window, instruction_txt=RSQ_txt)
+    endexp = instructions(window=Experiment.window, instruction_txt=end_txt)
+    # hide mouse
+    event.Mouse(visible=settings['mouse_visible'])
+
+    # wait trigger
+    trigger.draw()
+    Experiment.window.flip()
+    event.waitKeys(keyList=['5'])
+
+    # dummy volumes
+    timer = core.Clock()
+    fixation.show(timer, tr * dummy_vol)
+
+    # start the clock
+    timer = core.Clock()
+    # show fixation cross
+    fixation.show(timer, RS_length * 60)
+    if event.getKeys(keyList=['escape']):
+        print('user quit')
+        core.quit()
+
+    # instruction end
+    RSQ.show()
+    for q in questions:
+        question.set(q)
+        start_trial, score, rt = question.show(timer)
+        q['StartTime'] = start_trial
+        q['Rating'] = score
+        q['RT'] = rt
+        q['IDNO'] = experiment_info['Subject']
+        write_csv(experiment_info['DataFile'], headers, q)
+
+    # save files
+    logging.flush()
+    # change output files to read only
+    read_only(experiment_info['DataFile'])
+    read_only(experiment_info['LogFile'])
+
+    # quit
+    endexp.show(5)
+    Experiment.window.close()
+    core.quit()
---
+++ src/experiment.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+
+'''experiment.py
+experiemnt stimulus here
+'''
+from psychopy import core, data, gui, visual, event, logging
+from pyglet.window import key
+
+import os
+from src.fileIO import create_dir, load_instruction
+from random import uniform, shuffle
+
+import numpy as np
+
+sans = ['Arial','Gill Sans MT', 'Helvetica','Verdana'] #use the first font found on this list
+
+class Paradigm(object):
+    '''
+    Study paradigm
+    '''
+    def __init__(self, escape_key='esc', window_size=(1280, 720), color=0, *args, **kwargs):
+        self.escape_key = escape_key
+        self.trials = []
+        self.stims = {}
+
+        if window_size =='full_screen':
+            self.window = visual.Window(fullscr=True, color=color, units='pix', *args, **kwargs)
+        else:
+            self.window = visual.Window(size=window_size, color=color, allowGUI=True, units='pix', *args, **kwargs)
+
+class Text(object):
+    '''
+    show text in the middle of the screen
+    such as 'switch'
+    '''
+    def __init__(self, window, text, color):
+        '''Initialize a text stimulus.
+        Args:
+        window - The window object
+        text - text to display
+        duration - the duration the text will appear
+        keys - list of keys to press to continue to next stimulus. If None,
+                will automatically go to the next stimulus.
+        Additional args and kwargs are passed to the visual.TextStim
+        constructor.
+        '''
+        self.window = window
+        self.text = visual.TextStim(self.window, text=text, height=34, wrapWidth=1100, color=color, font=sans)
+
+    def show(self, clock, duration):
+        self.text.draw()
+        self.window.flip()
+        start_trial = clock.getTime()
+        core.wait(duration)
+        if event.getKeys(keyList=['escape']):
+            print('user quit')
+            core.quit()
+        return start_trial
+
+class Question(object):
+    '''
+    collect mind wandering report
+    '''
+    def __init__(self, window, questions, color):
+        '''Initialize a question stimulus.
+        Args:
+        window - The window object
+        questions - a list of dictionaries
+        keys - list of keys to press to continue to next stimulus. If None,
+                will automatically go to the next stimulus.
+        Additional args and kwargs are passed to the visual.TextStim
+        constructor.
+        '''
+        self.window = window
+        self.description = visual.TextStim(self.window, text=None, height=34, wrapWidth=1100, color=color, font=sans)
+        self.questions = questions
+        self.rating = visual.RatingScale(self.window, low=1, high=10, markerStart=5,
+                precision=10, tickMarks=[1, 10],
+                leftKeys='1', rightKeys='2', acceptKeys='4')
+
+    def set(self, trial):
+        self.description.setText(trial['Item'])
+        self.rating.setDescription(trial['Scale'])
+
+    def show(self, clock):
+        keyState=key.KeyStateHandler()
+        self.window.winHandle.push_handlers(keyState)
+        self.description.draw()
+        self.rating.draw()
+        self.window.flip()
+        start_trial = clock.getTime()
+
+        pos = self.rating.markerStart
+        inc = 0.1
+
+        while self.rating.noResponse:
+            if event.getKeys(keyList=['escape']):
+                print('user quit')
+                core.quit()
+
+            if keyState[key._1] is True:
+                pos -= inc
+            elif keyState[key._2] is True:
+                pos += inc
+
+            if pos > 9:
+                pos = 9
+            elif pos < 0:
+                pos = 0
+
+            self.rating.setMarkerPos(pos)
+            self.description.draw()
+            self.rating.draw()
+            self.window.flip()
+
+        score = self.rating.getRating()
+        rt = self.rating.getRT()
+        self.rating.reset()
+        return start_trial, score, rt
+
+class instructions(object):
+    '''
+    show instruction
+    '''
+    def __init__(self, window, instruction_txt):
+        self.window = window
+        self.instruction_txt = load_instruction(instruction_txt)
+
+        self.display = visual.TextStim(
+                window, text='default text', font=sans,
+                name='instruction',
+                pos=[-50,0], height=30, wrapWidth=1100,
+                color='black',
+                ) #object to display instructions
+
+    def show(self, duration=None):
+        # get instruction
+        for i, cur in enumerate(self.instruction_txt):
+            self.display.setText(cur)
+            self.display.draw()
+            self.window.flip()
+            if duration:
+                core.wait(duration)
+            else:
+                event.waitKeys(keyList=['4'])
+
+
+def subject_info(experiment_info):
+    '''
+    get subject information
+    return a dictionary
+    '''
+    dlg_title = '{} subject details:'.format(experiment_info['Experiment'])
+    infoDlg = gui.DlgFromDict(experiment_info, title=dlg_title)
+
+    experiment_info['Date'] = data.getDateStr()
+
+    file_root = ('_').join([experiment_info['Subject'],
+                            experiment_info['Experiment'], experiment_info['Date']])
+
+    experiment_info['DataFile'] = 'data' + os.path.sep + file_root + '.csv'
+    experiment_info['LogFile'] = 'data' + os.path.sep + file_root + '.log'
+
+    if infoDlg.OK:
+        return experiment_info
+    else:
+        core.quit()
+        print 'User cancelled'
+
+def event_logger(logging_level, LogFile):
+    '''
+    log events
+    '''
+    directory = os.path.dirname(LogFile)
+    create_dir(directory)
+
+    logging.console.setLevel(logging.WARNING)
+    logging.LogFile(LogFile, level=logging_level)
